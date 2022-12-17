80a009a23da6767c64894f5fb2a0b86e04f3d608
Simon Rose <simon.rose@542.digital>
2022-12-17 09:52:37 +0000

    fix: build

--- components/Nav/NavLink.tsx
+++ components/Nav/NavLink.tsx
@@ -12,9 +12,24 @@ export const NavLink: React.FC<NavLinkProps> = ({ href, label }) => {
 		<Link
 			href={href}
 			key={label}
-			className={`ml-4 px-3 py-2 rounded-md text-sm leading-5 font-medium text-white font-semibold hover:bg-gray-800 hover:text-white transition duration-150 ease-in-out cursor-pointer focus:outline-none focus:text-white ${
-				router.pathname === href ? 'underline' : ''
-			}`}
+			className={`
+				ml-4 
+				px-3 
+				py-2 
+				rounded-md 
+				text-sm 
+				leading-5 
+				font-medium
+				text-white 
+				font-semibold
+				hover:bg-gray-800
+				hover:text-white 
+				transition 
+				duration-150 
+				ease-in-out
+				cursor-pointer 
+				focus:outline-none
+			    focus:text-white ${router.pathname === href ? 'underline' : ''}`}
 		>
 			{label}
 		</Link>
