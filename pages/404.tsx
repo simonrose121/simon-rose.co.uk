1c25d69454a5b948dc070a40c718cbcc665a4b61
Simon Rose <simon.rose@542.digital>
2022-12-20 15:42:12 +0000

    feat: added 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,26 @@
+import { NextPage } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+import * as React from 'react';
+
+import { Layout } from 'Components';
+
+const CustomNotFound: NextPage = (): JSX.Element => (
+	<Layout>
+		<Head>
+			<title>Page Not Found | Simon Rose</title>
+			<link rel="icon" href="/favicon.svg" />
+		</Head>
+
+		<main className="container mx-auto mt-12 flex flex-col items-center">
+			<h2>
+				Oops, you&apos;ve taken a wrong turn. Back to{' '}
+				<Link href="/" className="underline inline">
+					home.
+				</Link>
+			</h2>
+		</main>
+	</Layout>
+);
+
+export default CustomNotFound;
