69907d0c27e2238c86a4a6d6b10d268259653ef2
Simon Rose <simon.rose@542.digital>
2023-03-22 09:24:43 +0000

    refactor: minor text update

--- content/education.ts
+++ content/education.ts
@@ -12,7 +12,7 @@ Games
 
 Supervisors: Dr Jacob Habgood and Dr Tim Jay
 
-My PhD focused on developing programming skills in primary-aged children, and my research is [regularly cited](https://www.researchgate.net/profile/Simon-Rose). My main output was a programming game called [Pirate Plunder](https://pirateplunder.app), that taught procedural abstraction to children aged 10 and 11. The game is available to play for free and is currently being used by the [Federal Center for Technological Education of Rio de Janeiro](https://en.wikipedia.org/wiki/Federal_Center_for_Technological_Education_of_Rio_de_Janeiro) as part of their programming curriculum.
+My PhD focused on developing programming skills in primary-aged children, and my research is still [regularly cited](https://www.researchgate.net/profile/Simon-Rose) despite moving away from academia. My main output was a programming game called [Pirate Plunder](https://pirateplunder.app), that taught procedural abstraction to children aged 10 and 11. The game is available to play for free and is currently being used by the [Federal Center for Technological Education of Rio de Janeiro](https://en.wikipedia.org/wiki/Federal_Center_for_Technological_Education_of_Rio_de_Janeiro) as part of their programming curriculum.
 
 I created several other web applications during my PhD, to test various computational thinking skills. I also taught part of the Computer Science curriculum (Scratch and Excel) to control groups.
 		`,
