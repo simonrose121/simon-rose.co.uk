4f1cae11acf7d5641d90870152e3504378c25d68
Simon Rose <simon.rose@542.digital>
2023-10-22 15:55:25 +0100

    fix: date

--- content/blog.ts
+++ content/blog.ts
@@ -13,7 +13,7 @@ export const posts: Array<BlogPostMeta> = [
 		id: '4-months-travelling',
 		title: '4 Months Travelling: Highlights, Low Points and Reflections of a Backpacking Adventure',
 		description: 'Highlights, low points and reflections of a 4 month backpacking adventure.',
-		date: new Date('2023-10-21'),
+		date: new Date('2023-10-22'),
 		path: '4-months-travelling',
 	},
 	{
